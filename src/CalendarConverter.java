97760191c2c98831aeec97f68cde02a62816b17e
adria <Konfitury2>
2019-07-30 23:09:35 +0200

    edycja klasy CaledarConverter

--- src/CalendarConverter.java
+++ src/CalendarConverter.java
@@ -1,20 +1,11 @@
 public class CalendarConverter {
     private static final int MONDAY = 1;
-    private static final int TEUSDAY = 2;
+    private static final int TUESDAY = 2;
     private static final int WEDNESDAY = 3;
     private static final int THURSDAY = 4;
     private static final int FRIDAY = 5;
     private static final int SATURDAY = 6;
     private static final int SUNDAY = 7;
-
-    private static int dayNumber;
-
-    public CalendarConverter() {
-    }
-
-    public static int getDayNumber() {
-        return dayNumber;
-    }
 
     public static String convertDayToString(int dayNumber){
         String dayName;
@@ -22,7 +13,7 @@ public class CalendarConverter {
             case CalendarConverter.MONDAY:
                 dayName = "poniedziałek";
                 break;
-            case CalendarConverter.TEUSDAY:
+            case CalendarConverter.TUESDAY:
                 dayName = "wtorek";
                 break;
             case CalendarConverter.WEDNESDAY:
